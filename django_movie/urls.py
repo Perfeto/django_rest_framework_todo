5475b078c9212b206bb69235a40d554c8942921b
Даниил Попов <denuly54@gmail.com>
2020-07-30 19:53:41 +0300

    remove unused imports

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,9 +27,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('testapp/', include('testapp.urls')),
-    path('openapi/', get_schema_view(
-        title="Your Project",
-        description="API for all things …",
-        version="1.0.0"
-    ), name='openapi-schema'),
 ]
