526e9e218efbf314ed34c4c739c3c414c3105c5a
Даниил Попов <denuly54@gmail.com>
2020-07-30 19:50:54 +0300

    add telelegrambot app

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'testapp',
     'corsheaders',
+    'telegrambot'
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movie',
+        'USER': 'postgres',
+        'PASSWORD': '1405',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
