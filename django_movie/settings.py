c771468009f22c2760eab6c779b618625a9895c5
Daniil Popov <denuly54@gmail.com>
2020-08-01 23:35:30 +0300

    added database for rasberry

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'movie',
         'USER': 'postgres',
